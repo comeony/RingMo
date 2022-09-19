252e1e215c82641e0a92ba1e39599bb24f4f4bf3
Lin-Bert <heqinglin336@qq.com>
2022-09-19 09:55:49 +0800

    init first.

---
+++ ringmo/src/models/eval/eval_engine.py
@@ -0,0 +1,86 @@
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""eval engine"""
+
+from mindspore import context
+
+from ringmo.src.models.eval.metric import ClassifyCorrect, ClassifyCorrectForDPMode, DistAccuracy
+
+
+class BasicEvalEngine():
+    """BasicEvalEngine"""
+
+    def __init__(self):
+        self.model = None
+
+    @property
+    def metric(self):
+        return None
+
+    @property
+    def eval_network(self):
+        return None
+
+    def compile(self, sink_size=-1):
+        pass
+
+    def eval(self):
+        pass
+
+    def set_model(self, model):
+        self.model = model
+
+    def get_result(self):
+        return None
+
+
+class EvelEngine(BasicEvalEngine):
+    """ImageNetEvelEngine"""
+
+    def __init__(self, net, eval_dataset, args):
+        super(EvelEngine, self).__init__()
+        self.eval_dataset = eval_dataset
+        is_data_parallel = context.get_auto_parallel_context(
+            "parallel_mode") == context.ParallelMode.DATA_PARALLEL
+        use_moe = args.moe_config.expert_num > 1
+        if is_data_parallel:
+            self.dist_eval_network = ClassifyCorrectForDPMode(net, use_moe)
+        else:
+            self.dist_eval_network = ClassifyCorrect(net, use_moe)
+        self.args = args
+        self.outputs = None
+        self.model = None
+
+    @property
+    def metric(self):
+        return {'acc': DistAccuracy(batch_size=self.args.train_config.batch_size,
+                                    device_num=self.args.device_num,
+                                    samples_num=self.args.finetune_dataset.samples_num)}
+
+    @property
+    def eval_network(self):
+        return self.dist_eval_network
+
+    def eval(self):
+        self.outputs = self.model.eval(self.eval_dataset)
+
+    def get_result(self):
+        return self.outputs["acc"]
+
+
+def build_eval_engine(net, eval_dataset, args):
+    """get_eval_engine"""
+    eval_engine = EvelEngine(net, eval_dataset, args)
+    return eval_engine
---
+++ ringmo/src/tools/helper.py
@@ -0,0 +1,75 @@
+import sys
+
+import yaml
+import numpy as np
+import aicc_tools as ac
+
+from mindspore import context
+from mindspore.parallel import set_algo_parameters
+from mindspore.parallel._cost_model_context import _set_multi_subgraphs
+
+
+def build_context(args):
+    profile_cb = None
+    if args.train_config.profile and args.use_parallel:
+        cfts_1 = ac.CFTS(**args.aicc_config)
+        profile_cb = cfts_1.profile_monitor(start_step=1, stop_step=5)
+
+    local_rank, device_num = ac.context_init(seed=args.seed, use_parallel=args.use_parallel,
+                                             context_config=args.context, parallel_config=args.parallel)
+    context.set_context(max_device_memory="30GB")
+    # context.set_context(env_config_path=args.mem_reuse)
+    set_algo_parameters(elementwise_op_strategy_follow=True, fully_use_devices=False)
+    _set_multi_subgraphs()
+
+    args.device_num = device_num
+    args.local_rank = local_rank
+    args.logger = ac.get_logger()
+    args.logger.info("model config: {}".format(args))
+
+    # init cfts
+    cfts = ac.CFTS(**args.aicc_config, rank_id=local_rank)
+
+    if args.parallel.get("strategy_ckpt_load_file"):
+        args.parallel["strategy_ckpt_load_file"] = cfts.get_checkpoint(args.parallel.get("strategy_ckpt_load_file"))
+        context.set_auto_parallel_context(strategy_ckpt_load_file=args.parallel["strategy_ckpt_load_file"])
+
+    if args.train_config.profile and not args.use_parallel:
+        cfts_2 = ac.CFTS(**args.aicc_config)
+        profile_cb = cfts_2.profile_monitor(start_step=1, stop_step=5)
+    return cfts, profile_cb
+
+
+def str2bool(b):
+    if b.lower() in ["false"]:
+        output = False
+    elif b.lower() in ["true"]:
+        output = True
+    else:
+        raise Exception("Invalid Bool Value")
+    return output
+
+
+def parse_with_config(parser):
+    """Parse With Config"""
+    args = parser.parse_args()
+    if args.config is not None:
+        config_args = yaml.load(open(args.config), Loader=yaml.FullLoader)
+        override_keys = {arg[2:].split('=')[0] for arg in sys.argv[1:]
+                         if arg.startswith('--')}
+        for k, v in config_args.items():
+            if k not in override_keys:
+                setattr(args, k, v)
+    del args.config
+    return args
+
+
+def count_params(net):
+    """Count number of parameters in the network
+    Args:
+        net (mindspore.nn.Cell): Mindspore network instance
+    Returns:
+        total_params (int): Total number of trainable params
+    """
+    total_params = [np.prod(param.shape) for param in net.trainable_params()]
+    return sum(total_params) // 1000000
